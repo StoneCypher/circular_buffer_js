7f0c630ee7608a100e6bc249dfcb3a68c14b5205
John Haugeland <stonecypher@gmail.com>
2020-12-17 16:02:30 -0800

    Rollup bundling.  Output is unminified esm, iife, cjs.

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+
+const es6_config = {
+
+  input   : 'build/es6/circular_buffer.js',
+
+  output  : {
+    file   : 'dist/circular_buffer.esm.js',
+    format : 'es',
+    name   : 'circular_buffer'
+  }
+
+};
+
+
+
+
+
+const cjs_config = {
+
+  input   : 'build/es6/circular_buffer.js',
+
+  output  : {
+    file   : 'dist/circular_buffer.cjs.js',
+    format : 'cjs',
+    name   : 'circular_buffer'
+  }
+
+};
+
+
+
+
+
+const iife_config = {
+
+  input   : 'build/es6/circular_buffer.js',
+
+  output  : {
+    file   : 'dist/circular_buffer.iife.js',
+    format : 'iife',
+    name   : 'circular_buffer'
+  }
+
+};
+
+
+
+
+
+export default [ es6_config, cjs_config, iife_config ];
