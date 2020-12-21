ef965d0e8ead5273d4f01a57a974e97abe6222bc
John Haugeland <stonecypher@gmail.com>
2020-12-21 12:57:30 -0800

    simpler script based around artifacts

--- archive_docs_by_version.js
+++ archive_docs_by_version.js
@@ -4,17 +4,7 @@ const fs      = require('fs'),
 
 const rimraf  = require('rimraf');
 
-const package = require('../../package.json');
-
-
-
-
-
-const docsdir = './docs/docs/',
-      tempdir = '../cbjs_docs_temp/',
-      currdir = './docs/current/',
-      versdir = `./docs/v${package.version}/`,
-      pkg_f   = `${tempdir}package.json`;
+const base    = require('./base-package.json');
 
 
 
@@ -44,33 +34,7 @@ function copyFolderSync(from, to) {
 
 
 
-const logline = `Currently targeting:
-  docsdir : ${docsdir}
-  tempdir : ${tempdir}
-  currdir : ${currdir}
-  versdir : ${versdir}
-  pkg_f   : ${pkg_f}
-`;
-
-if (process.env['STEP'] === 'TEMPORARY') {       // Are we at the pre-other-checkout step?
-  console.log(logline);
-  copyFolderSync(docsdir, tempdir);                // Put the generated docs somewhere they can be re-gotten
-  fs.copyFileSync('./package.json', pkg_f);        // cache package.json
-
-} else if (process.env['STEP'] === 'ARCHIVE') {  // Are we at the store the post-checkout docs step?
-  console.log(logline);
-  rimraf.sync(currdir);                            // destroy the previous `current` directory
-  fs.copyFileSync(pkg_f, './package.json');        // restore baseline package.json
-  copyFolderSync(tempdir, currdir);                // clone into `current` (clone remakes dir)
-  copyFolderSync(tempdir, versdir);                // clone into version directory also
-
-} else {                                         // Otherwise where are we?
-  throw new Error('No process step, failing');
-
-}
-
-
-
-
+rimraf.sync(`./docs/${base.version}/`);
+copyFolderSync('./docs/current/', `./docs/${base.version}/`)
 
 console.log(`# Cloned current docs to archival directory for ${package.version}; finished`);
