ec02720be251cc8ace2ac0671f372d62c50004da
John Haugeland <stonecypher@gmail.com>
2021-01-25 22:25:52 -0800

    regen

--- dist/circular_buffer.iife.js
+++ dist/circular_buffer.iife.js
@@ -1,7 +1,7 @@
 var circular_buffer = (function (exports) {
     'use strict';
 
-    const version = '0.15.6';
+    const version = '0.15.8';
 
     class circular_buffer {
         constructor(uCapacity) {
