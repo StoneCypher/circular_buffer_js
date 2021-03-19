38f1661249e37b87e49f9e5aae5d7aa14666c60d
John Haugeland <stonecypher@gmail.com>
2021-03-19 12:31:59 -0700

    bump and rebuild

--- dist/circular_buffer.iife.js
+++ dist/circular_buffer.iife.js
@@ -1,7 +1,7 @@
 var circular_buffer = (function (exports) {
     'use strict';
 
-    const version = '0.15.8';
+    const version = '0.15.9';
 
     class circular_buffer {
         constructor(uCapacity) {
